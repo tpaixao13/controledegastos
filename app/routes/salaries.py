9cfd69111c36dd585aae4c8659a251e6c3c3814b
tpaixao13 <tpaixao13@gmail.com>
2026-04-29 14:24:34 -0300

    auto(Edit): salaries.py"

--- app/routes/salaries.py
+++ app/routes/salaries.py
@@ -3,6 +3,7 @@ from sqlalchemy import func
 from app import db
 from app.models import User, Salary
 from app.forms import SalaryForm
+from app.utils import tenant_users, tenant_user_ids
 from datetime import datetime
 
 salaries_bp = Blueprint('salaries', __name__, url_prefix='/salaries')
