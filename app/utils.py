35fe53b280c09cc546314f2c59d72db55e59eda2
tpaixao13 <tpaixao13@gmail.com>
2026-04-29 16:52:31 -0300

    auto(Edit): utils.py"

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,12 @@
 from flask import session
 from app.models import User
 
+MONTH_NAMES_SHORT = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+                     'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+
+MONTH_NAMES_FULL = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
 
 def tenant_users():
     tid = session.get('tenant_id')
