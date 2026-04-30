c846e044a544ca2e7b0561240e835124a9f38491
tpaixao13 <tpaixao13@gmail.com>
2026-04-30 14:41:36 -0300

    auto(Edit): api.py"

--- app/routes/api.py
+++ app/routes/api.py
@@ -7,7 +7,7 @@ from app import db
 from app.models import Expense, Salary, User, Investment
 from app.utils import (tenant_user_ids, tenant_users, MONTH_NAMES_SHORT,
                        _fetch_json, get_selic_rate, rate_suggestions, month_offset,
-                       sum_expenses_month, sum_salaries_month)
+                       sum_expenses_month, sum_salaries_month, USER_PALETTE)
 
 api_bp = Blueprint('api', __name__, url_prefix='/api/chart')
 
