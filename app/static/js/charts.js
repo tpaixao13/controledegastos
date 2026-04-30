d78f1b66cde69fb4234d1d5327b0a47a439879e4
tpaixao13 <tpaixao13@gmail.com>
2026-04-30 14:41:52 -0300

    auto(Edit): charts.js"

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -113,8 +113,8 @@ document.addEventListener('DOMContentLoaded', function () {
             {
               label: 'Gastos',
               data: d.gastos,
-              backgroundColor: ['rgba(13,110,253,0.7)', 'rgba(220,53,69,0.7)'],
-              borderColor: ['#0d6efd', '#dc3545'],
+              backgroundColor: d.colors,
+              borderColor: d.border_colors,
               borderWidth: 1,
             }
           ]
