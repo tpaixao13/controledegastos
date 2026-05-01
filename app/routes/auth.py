50f4837c5969ac44ce5b121d6f477efa6a107491
tpaixao13 <tpaixao13@gmail.com>
2026-05-01 11:51:09 -0300

    auto(Edit): auth.py"

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,7 +4,8 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request,
 from werkzeug.utils import secure_filename
 from app import db
 from app.models import User, Tenant
-from app.forms import LoginForm, ChangePasswordForm, AvatarForm, RegisterTenantForm, AddMemberForm
+from app.forms import LoginForm, ChangePasswordForm, AvatarForm, RegisterTenantForm, AddMemberForm, TelegramConfigForm
+from app.utils import build_daily_reminder, send_telegram_message
 
 auth_bp = Blueprint('auth', __name__)
 
