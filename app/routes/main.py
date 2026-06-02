8bda5d1f289fd9813f703b4b2513a363dc6e2950
tpaixao13 <tpaixao13@gmail.com>
2026-06-02 14:09:27 -0300

    auto(Edit): main.py"

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request
 from sqlalchemy import func
 from app import db
-from app.models import User, Expense, Salary
+from app.models import User, Expense, Salary, CreditCard
 from app.utils import (tenant_users, tenant_user_ids, MONTH_NAMES_FULL,
                        month_offset, sum_expenses_month, sum_salaries_month,
                        user_color_map, get_month_year)
+from app.services.insight_service import generate_insights
 from datetime import datetime
 
 main_bp = Blueprint('main', __name__)
